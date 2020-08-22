95fe2e3de075ccd99b3731f384c1d509c354d0be
Tomás Castillo <tcastilloboireau@gmail.com>
2020-08-22 16:32:25 -0300

    validation for ticket types on step one

--- src/pages/step-one-page.js
+++ src/pages/step-one-page.js
@@ -78,7 +78,7 @@ class StepOnePage extends React.Component {
         let {summit, order} = this.props;
         let now = this.props.getNow();
         // filter tickets types
-        let ticketsTypesToSell = summit.ticket_types.filter( tt =>
+        let ticketsTypesToSell = (Object.entries(summit).length === 0 && summit.constructor === Object) ? [] : summit.ticket_types.filter( tt =>
             // if ticket does not has sales start/end date set could be sell all the registration period
             (tt.sales_start_date == null && tt.sales_end_date == null) ||
             (now >= tt.sales_start_date && now <= tt.sales_end_date)
