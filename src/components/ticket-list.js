2d5e00126f0a604e95a14efb16fe901f5bb640fb
Román Gutierrez <roman.gutierrez@gmail.com>
2020-11-04 17:16:36 -0300

    Ui adjustments (#20)
    * ui adjustments
    
    * Remove Address Collection from Registration
    
    * Remove Address Collection from Registration
    
    * Remove Address Collection from Registration
    
    * Remove Address Collection from Registration
    
    * Fix: Too many commas in dates and order number not contained on registration tickets

--- src/components/ticket-list.js
+++ src/components/ticket-list.js
@@ -132,7 +132,7 @@ class TicketList extends React.Component {
         let startYear = startDate.substring(startDate.length, startDate.length-4);
         let endYear = endDate.substring(endDate.length, endDate.length-4);
         if (startYear === endYear) startDate = startDate.substring(0, startDate.length-4);
-        endDate = endDate.substring(0, endDate.length-5) + ', ' + endDate.substring(endDate.length-4);
+        endDate = endDate.substring(0, endDate.length-6) + ', ' + endDate.substring(endDate.length-4);
         let summitDate = `${startDate} - ${endDate}`;
         return summitDate;
       } else {
